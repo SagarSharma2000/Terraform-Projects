f0a0ec67f372cf4301a2c606c0966a8c5cb9535e
Sagar <sssagar2500@gmail.com>
2026-08-26 16:36:44 +0530

    added tf-backend

---
+++ Terraform/tf-backend/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.61.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-bucket-45114005a5c60a07"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+}
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_instance" {
+    ami           = "ami-01a00762f46d584a1" # Ubuntu AMI ID for ap-south-1 region (as of the time of writing)
+    instance_type = "t3.micro" # Free tier eligible instance type    
+  
+  tags = {
+    Name = "MyInstance"
+  }
+}
+
